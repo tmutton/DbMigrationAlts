b31aa8833200732c2418d7db645f1df9a25ac3f0
Thomas Mutton <tmutton@businesssystemsuk.com>
2017-06-06 16:05:25 +0100

    finished dbup

--- src/DbUp/Program.cs
+++ src/DbUp/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DbUp
+﻿namespace DbUp
 {
+    using System;
+    using System.Reflection;
+
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            Console.WriteLine("Testing DbUp");
+
+            const string ConnectionString = "Server=localhost;Port=5432;Database=dbMigration;User Id=postgres;Password=postgres;";
+
+            EnsureDatabase.For.PostgresqlDatabase(ConnectionString);
+
+            var upgrader =
+                DeployChanges.To
+                    .PostgresqlDatabase(ConnectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+#if DEBUG
+                Console.ReadLine();
+#endif
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            Console.WriteLine("0");
+            return 0;
         }
     }
 }
---
+++ src/DbUp/Scripts/Script0001-Create-Users-Table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE public.users
+(
+    Id uuid NOT NULL,
+	Username character varying(256) NOT NULL,
+	PasswordHash character varying(256),
+	SecurityStamp character varying(256),
+	Email character varying(256) DEFAULT NULL::character varying,
+	EmailConfirmed boolean NOT NULL DEFAULT false,
+    PRIMARY KEY (Id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.users
+    OWNER to postgres;
